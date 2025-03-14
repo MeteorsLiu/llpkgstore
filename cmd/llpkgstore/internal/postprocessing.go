10f135a42f1afdf6fe8560a575eec25febcc906e
Meteor <coopticks@outlook.com>
2025-03-14 11:27:03 +0000

    update

--- cmd/llpkgstore/internal/postprocessing.go
+++ cmd/llpkgstore/internal/postprocessing.go
@@ -1,21 +1,18 @@
 package internal
 
 import (
-	"log"
-
 	"github.com/MeteorsLiu/llpkgstore/actions"
 	"github.com/spf13/cobra"
 )
 
 var postProcessingCmd = &cobra.Command{
-	Use:   "post-processing",
+	Use:   "postprocessing",
 	Short: "Verify a PR",
 	Long:  ``,
 	Run:   runPostProcessingCmd,
 }
 
 func runPostProcessingCmd(_ *cobra.Command, _ []string) {
-	log.Println("run post processing")
 	actions.NewDefaultClient().Release()
 }
 
